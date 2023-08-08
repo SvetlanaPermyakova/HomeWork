294f4a2d1b66ffbc84e9d633f9eb5710bcec587a
Roman Permyakov <info@presstomsk.ru>
2023-08-08 22:11:51 +0700

    Ex3

---
+++ Exercise3/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercise3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение а: ");
+            string? a = Console.ReadLine();
+            int num1 = Convert.ToInt32(a);
+
+            if (num1 % 2 == 0)
+            {
+                Console.WriteLine(num1 + " - чётное");
+            }
+            else
+            {
+                Console.WriteLine(num1 + " - нечётное");
+            }
+        }
+    }
+}
