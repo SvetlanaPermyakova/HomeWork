9aad0e4f22879e23f25018de9f782b86b5f50c9b
Roman Permyakov <info@presstomsk.ru>
2023-08-08 22:32:14 +0700

    Ex4

---
+++ Exercise4/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercise4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение N: ");
+            string? N = Console.ReadLine();
+            int num1 = Convert.ToInt32(N);
+
+            for (int i = 1; i <= num1; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
