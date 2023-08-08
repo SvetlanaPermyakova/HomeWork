87ef933521509b6065639685ace95e357e8452ac
Roman Permyakov <info@presstomsk.ru>
2023-08-08 21:45:19 +0700

    Добавьте файлы проекта.

---
+++ Exercise1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int max;
+
+            Console.WriteLine("Введите значение а: ");
+            string? a = Console.ReadLine();
+            int num1 = Convert.ToInt32(a);
+
+            Console.WriteLine("Введите значение b: ");
+            string? b = Console.ReadLine();
+            int num2 = Convert.ToInt32(b);
+
+            if (num1 > num2)
+            { 
+                max = num1;
+            }
+            else
+            {
+                max = num2;
+            } 
+
+            Console.WriteLine("Максимальное значение равно " + max);
+        }        
+    }
+
+    
+}
